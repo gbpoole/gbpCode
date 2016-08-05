c48f052038e9dce7486e224bfa3256c94b05e1b0
Gregory Poole <gbpoole@gmail.com>
2016-08-05 14:13:44 +1000

    Fixed a slight round-off problem in float_to_text().

--- gbpLib/gbpParse/core/float_to_text.c
+++ gbpLib/gbpParse/core/float_to_text.c
@@ -11,7 +11,7 @@ void float_to_text(float number,int n_sig_fig,char *result){
    float number2=number1;
    for(int i_sig_fig=0;i_sig_fig<n_sig_fig;i_sig_fig++)
       number2*=10;
-   int number3=(int)number2;
+   int number3=(int)(number2+0.5);
    char format_string[32];
    sprintf(format_string,"%%dpt%%0%dd",n_sig_fig);
    sprintf(result,format_string,number0,number3);
