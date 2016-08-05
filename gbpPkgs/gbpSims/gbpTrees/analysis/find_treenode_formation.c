c4ae2cf02076baef29f132723b529177ec1a5db3
Gregory Poole <gbpoole@gmail.com>
2016-08-05 15:27:48 +1000

    Halo formation times now computed in gbpTrees using inclusive particle counts.

--- gbpPkgs/gbpSims/gbpTrees/analysis/find_treenode_formation.c
+++ gbpPkgs/gbpSims/gbpTrees/analysis/find_treenode_formation.c
@@ -15,20 +15,20 @@ int find_treenode_formation(tree_info       *trees,
                             tree_node_info **halo_formation){
    (*halo_formation)=halo;
    if(halo!=NULL){
-      int n_p_target=(int)(fraction*(double)(halo->n_particles_peak));
+      int n_p_target=(int)(fraction*(double)(halo->n_particles_inclusive_peak));
       tree_node_info *current_halo=halo;
       while(current_halo!=NULL){
-         if(current_halo->n_particles_peak>=n_p_target)
+         if(current_halo->n_particles_inclusive_peak>=n_p_target)
             (*halo_formation)=current_halo;
          else break;
          // Because progenitors could be ordered many ways, find the most massive one
          //    and scan along that
          tree_node_info *most_massive_progenitor=current_halo->progenitor_first;
          if(most_massive_progenitor!=NULL){
-            int n_p_mm=most_massive_progenitor->n_particles_peak;
+            int n_p_mm=most_massive_progenitor->n_particles_inclusive_peak;
             current_halo=most_massive_progenitor->progenitor_next;
             while(current_halo!=NULL){
-               int n_p_i=current_halo->n_particles_peak;
+               int n_p_i=current_halo->n_particles_inclusive_peak;
                if(n_p_i>n_p_mm){
                   most_massive_progenitor=current_halo;
                   n_p_mm=n_p_i;
