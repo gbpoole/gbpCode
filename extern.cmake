75f376e2165f63516402144bf2f99b40784c1429
Gregory Poole <gbpoole@gmail.com>
2018-02-11 13:21:49 +1100

    Removed 'extern' directory.

--- extern.cmake
+++ extern.cmake
@@ -1,5 +1,4 @@
 # Configure any external projects, submodules, etc.
 macro(local_extern cur_dir )
-    # gbpBuild provides the boilerplate for this project's development environment
-    add_external_submodule( ${cur_dir} "gbpBuild" ${cur_dir}"/gbpBuild/README.md" )
+    # Use 'add_external_submodule()' here to ensure that submodules are added
 endmacro()
